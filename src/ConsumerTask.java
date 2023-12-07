4c7224a72849a6af0253e2cf40b0c20bcf191801
v sotnikov <sotnikov@rosplatforma.ru>
2023-12-07 18:25:55 +0300

    07122023commit

---
+++ src/ConsumerTask.java
@@ -0,0 +1,19 @@
+public class ConsumerTask implements Runnable {
+    private TransferObject transferObject;
+    protected volatile boolean stopped;
+
+    public ConsumerTask(TransferObject transferObject) {
+        this.transferObject = transferObject;
+        new Thread(this, "ConsumerTask").start();
+    }
+
+    public void run() {
+        while (!stopped) {
+            transferObject.get();
+        }
+    }
+
+    public void stop() {
+        stopped = true;
+    }
+}
